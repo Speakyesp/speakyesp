4a54c284be0607fefbd8e98034c02583170b3c1a
Speakyesp <161930388+Speakyesp@users.noreply.github.com>
2024-03-02 21:49:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -174,31 +174,24 @@ class ChatApp extends React.Component {
         }
     };
 
-   uploadImage = (file) => {
+  uploadImage = (file) => {
     // Upload the image file to Firebase Storage
     return new Promise((resolve, reject) => {
-        // Get a reference to Firebase Storage
-        const storageRef = firebase.storage().ref();
-        
-        // Specify the path where the image will be stored
+        const storageRef = firebase.storage();
         const imageRef = storageRef.child(`images/${file.name}`);
-        
-        // Upload the image file to Firebase Storage
         imageRef.put(file)
             .then(snapshot => {
-                // Once upload is complete, get the download URL of the image
                 return snapshot.ref.getDownloadURL();
             })
             .then(downloadURL => {
-                // Resolve the promise with the download URL
                 resolve(downloadURL);
             })
             .catch(error => {
-                // If any error occurs during the upload process, reject the promise with the error
                 reject(error);
             });
     });
 };
+
 
     sendMessageWithImage = (newMessage, user, imageURL, repliedMessage) => {
         const db = getDatabase();
