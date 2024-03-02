5c4458cb3ffc717519704daa488e11f041ed900e
Speakyesp <161930388+Speakyesp@users.noreply.github.com>
2024-03-02 16:23:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -273,7 +273,7 @@ class ChatApp extends React.Component {
                             />
                             <button className="login-button" onClick={this.handleEmailLogin}>Login</button>
                         </div>
-                    )}
+                   
                 
                 </div>
                 {/* <button className="scroll-to-end-button" onClick={this.handleScrollToEnd}>Scroll to End</button> */}
