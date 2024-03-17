b646bd009a963241f1699d073d9015eefc22a250
Speakyesp <161930388+Speakyesp@users.noreply.github.com>
2024-03-17 23:11:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCiSF3tS1aexfPtGGr9OAWIYBK4OLRehoI",
-    authDomain: "chatus-6b5b6.firebaseapp.com",
-    databaseURL: "https://chatus-6b5b6-default-rtdb.firebaseio.com",
-    projectId: "chatus-6b5b6",
-    storageBucket: "chatus-6b5b6.appspot.com",
-    messagingSenderId: "673849113997",
-    appId: "1:673849113997:web:7251b7d022e314a63691c4",
-    measurementId: "G-556CMZLY10"
+  apiKey: "AIzaSyBoWWHe1Q25I1yEF3o-nf9ae5G9vgUOUfo",
+  authDomain: "speaktest-19dd6.firebaseapp.com",
+  databaseURL: "https://speaktest-19dd6-default-rtdb.firebaseio.com",
+  projectId: "speaktest-19dd6",
+  storageBucket: "speaktest-19dd6.appspot.com",
+  messagingSenderId: "86205945597",
+  appId: "1:86205945597:web:fde45e4e8c868643b99094",
+  measurementId: "G-45LGLDX7CX"
 };
 
 // Initialize Firebase
